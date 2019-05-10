7e1772b4773faf7dd41c756100cf79e0ef5c3734
Gowtham Behara <gothsiddu@gmail.com>
2019-05-10 12:13:14 +0530

    Capture background

--- invisible_cloak.py
+++ invisible_cloak.py
@@ -1,2 +1,8 @@
 import cv2
 import numpy as np
+
+capture, background = cv2.VideoCapture(0), None
+for itr in range(30):
+    _, background = capture.read()
+
+background = np.flip(background, axis=1)
